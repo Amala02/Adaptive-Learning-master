75d05b273451b6f08cd1474a40036a55b73e1cfc
Amala-KSD <amala.shwetah.louis.kishore@ksdadvisory.com>
2025-03-17 23:53:54 +0530

    UI changes and bugs fixed

--- Adaptive-Learning-master/learning/discussions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Post)
-admin.site.register(BlogComment)
-admin.site.register(Reward)
--- Adaptive-Learning-master/learning/leaderboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.conf.urls.static import static
-from . import views
-
-app_name = 'leaderboard'
-
-urlpatterns = [
-    path('', views.leaderHome, name="leader-home"),
-]
